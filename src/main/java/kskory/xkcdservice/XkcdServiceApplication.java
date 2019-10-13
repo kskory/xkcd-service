b5bfc9e1f38ae468cc43f5f97564f787122bd716
Kacper Skory <kacper.skory@corsearch.com>
2019-10-13 11:09:34 +0200

    initial commit

---
+++ src/main/java/kskory/xkcdservice/XkcdServiceApplication.java
@@ -0,0 +1,13 @@
+package kskory.xkcdservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XkcdServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XkcdServiceApplication.class, args);
+	}
+
+}
